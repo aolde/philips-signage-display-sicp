e219f5f29c973f19a63138caa75761a770605161
Andreas Oldeskog <andreas@oldeskog.se>
2019-03-03 16:13:36 +0100

    Example code in Program.cs

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Drawing;
+using System.Net;
+using System.Threading;
+
+namespace PhilipsSignageDisplaySicp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ipAddress = IPAddress.Parse("192.168.1.132");
+            var client = new PhilipsSicpClient(ipAddress);
+
+            client.GetModelInformation();
+        }
+
+        static void LoopColors(PhilipsSicpClient client)
+        {
+            Color[] colors = new[] { Color.Aqua, Color.Green, Color.Red };
+            int index = 0;
+
+            while (true)
+            {
+                var color = colors[index % colors.Length];
+                Console.WriteLine("Setting color {0}", color);
+                client.SetLedStrip(true, color);
+                index++;
+                Thread.Sleep(1500);
+            }
+        }
+    }
+}
+
+
